db3bf18572ddd1932aa727144119518396351bfd
RohitBCA456 <ry2281104@gmail.com>
2026-04-25 20:43:39 +0530

    fix: remove unused stats and projectStats mocks from notification tests

--- Backend/src/test/unit test/notificationControllerTesting/sendNotification.test.js
+++ Backend/src/test/unit test/notificationControllerTesting/sendNotification.test.js
@@ -16,23 +16,29 @@ describe("Send Notification Controller", () => {
 
   it("should successfully send notification", async () => {
     const sendEmailMockFn = mock.fn(async () => Promise.resolve(true));
-    const statsMockFn = mock.fn(async () => [{ total: 10 }]);
-    const projectStatsMokcFn = mock.fn(async () => [{ totalSent: 5, successRate: 50 }]);
-    const emitStatsMock = mock.fn();
-    const redisMock = { hSet: mock.fn(async () => {}), expire: mock.fn(async () => {}), hIncrBy: mock.fn(async () => {}) };
+    const hIncrByMock = mock.fn(async () => {});
 
-    const { sendNotification } = await esmock("../../../controllers/notification.controller.js", {
-      "../../../workers/email.worker.js": { sendEmail: sendEmailMockFn },
-      "../../../services/stats.service.js": { getNotificationStats: statsMockFn },
-      "../../../services/projectStats.service.js": { getProjectStat: projectStatsMokcFn },
-      "../../../config/socket.js": { emitStats: emitStatsMock },
-      "../../../config/redis.js": { client: redisMock }
-    });
+    const { sendNotification } = await esmock(
+      "../../../controllers/notification.controller.js",
+      {
+        "../../../workers/email.worker.js": { sendEmail: sendEmailMockFn },
+        "../../../config/redis.js": {
+          client: {
+            hIncrBy: hIncrByMock,
+          },
+        },
+      }
+    );
 
     await sendNotification(fakeNotification);
 
     assert.strictEqual(sendEmailMockFn.mock.callCount(), 1);
-    assert.strictEqual(statsMockFn.mock.callCount(), 1);
+    assert.strictEqual(
+      sendEmailMockFn.mock.calls[0].arguments[0],
+      fakeNotification.to
+    );
+
+    assert.strictEqual(hIncrByMock.mock.callCount(), 1);
   });
 
   it("should return 500 if internal server error occurs", async () => {
@@ -40,26 +46,36 @@ describe("Send Notification Controller", () => {
       throw new Error("SMTP Error");
     });
 
-    const notificationStatusUpdateMock = mock.method(Notification, "findByIdAndUpdate", async () => ({}));
+    const notificationStatusUpdateMock = mock.method(
+      Notification,
+      "findByIdAndUpdate",
+      async () => ({})
+    );
 
-    const { sendNotification } = await esmock("../../../controllers/notification.controller.js", {
-      "../../../workers/email.worker.js": { sendEmail: sendEmailErrorMock },
-      "../../../services/stats.service.js": { getNotificationStats: mock.fn() },
-      "../../../services/projectStats.service.js": { getProjectStat: mock.fn() },
-      "../../../config/socket.js": { emitStats: mock.fn() },
-      "../../../config/redis.js": { client: { hSet: mock.fn(), expire: mock.fn() } }
-    });
+    const { sendNotification } = await esmock(
+      "../../../controllers/notification.controller.js",
+      {
+        "../../../workers/email.worker.js": { sendEmail: sendEmailErrorMock },
+        "../../../config/redis.js": {
+          client: {
+            hIncrBy: mock.fn(async () => {}),
+          },
+        },
+      }
+    );
 
     try {
       await sendNotification(fakeNotification);
+      assert.fail("Should have thrown an error");
     } catch (error) {
       assert.strictEqual(error.message, "SMTP Error");
     }
 
     assert.strictEqual(notificationStatusUpdateMock.mock.callCount(), 1);
-    
     const callArgs = notificationStatusUpdateMock.mock.calls[0].arguments;
     assert.strictEqual(callArgs[0], fakeNotification._id);
     assert.strictEqual(callArgs[1].status, "failed");
+
+    notificationStatusUpdateMock.mock.restore();
   });
 });
